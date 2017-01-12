f98eb5c9eef0ac29b0b2530fa042c59895ab03bf
Mansoor Ali <Mansoor.hussain776@gmail.com>
2017-01-12 21:23:02 +1100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'pages/contact_us'
   get 'pages/opening_hours'
   
-	root "pages#index"
+	root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
